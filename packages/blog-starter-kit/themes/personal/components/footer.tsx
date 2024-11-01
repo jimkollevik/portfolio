44726a2bbce3095474afbf290f9d9cc4633b0bd8
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-01 08:22:45 +0100

    Update footer.tsx

--- packages/blog-starter-kit/themes/personal/components/footer.tsx
+++ packages/blog-starter-kit/themes/personal/components/footer.tsx
@@ -5,7 +5,10 @@ export const Footer = () => {
 
 	return (
 		<footer className="border-t pt-10 text-sm text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
-			&copy; {new Date().getFullYear()} {publication.title}
+			<ul>
+        <li>&copy; {new Date().getFullYear()} {publication.title}</li>
+        <li>Trying to be 1% everyday</li>
+      </ul>
 		</footer>
 	);
 };
