78671576b6fc3b9fbbf85ea2a26ff745856128b7
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-05 20:33:42 +0100

    Update index.tsx

--- packages/blog-starter-kit/themes/personal/pages/index.tsx
+++ packages/blog-starter-kit/themes/personal/pages/index.tsx
@@ -86,16 +86,37 @@ export default function Index({ publication, initialPosts, initialPageInfo }: Pr
 				<Container className="mx-auto flex max-w-3xl flex-col">
 				<div className="grid grid-cols-2 gap-4">
 					<div className="">
-						<ul>
-							<li>Jim Kollevik</li>
-							<li>Product Owner</li>
-						</ul>
+						<p>
+							Hi, I'm Jim<br>
+							Product Owner
+						</p>
 					</div>
 					<div className="">	
-						<span>
+						<p>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a magna  nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
 							Suspendisse tincidunt a odio ac scelerisque.
-						</span>
+						</p>
+						<p>
+							Sed dignissim in magna quis facilisis. Donec malesuada justo nec lacinia lobortis. Nunc ac nulla et leo egestas fermentum a et magna. Sed sit amet convallis quam.
+						</p>
+						<p>
+							Curabitur mi neque, pellentesque a nulla id, porttitor aliquam nunc.
+						</p>
+						<p><a href="" >jim.kollevik@gmail.com</a></p>
+					</div>
+					<div className="">	
+						Work
+					</div>
+					<div>
+						{posts.length > 0 && <MinimalPosts context="home" posts={posts} />}
+						{!loadedMore && pageInfo.hasNextPage && pageInfo.endCursor && (
+							<button onClick={loadMore}>
+								Load more
+							</button>
+						)}
+						{loadedMore && pageInfo.hasNextPage && pageInfo.endCursor && (
+							<Waypoint onEnter={loadMore} bottomOffset={'10%'} />
+						)}
 					</div>
 						<div>
 							Posts
