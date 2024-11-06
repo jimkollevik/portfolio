da3b78c6d61e6b8eca4e5ebfdf6760819ff86322
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-06 20:49:59 +0100

    Update index.tsx

--- packages/blog-starter-kit/themes/personal/pages/index.tsx
+++ packages/blog-starter-kit/themes/personal/pages/index.tsx
@@ -83,7 +83,7 @@ export default function Index({ publication, initialPosts, initialPageInfo }: Pr
 						}}
 					/>
 				</Head>
-				<Container className="mx-auto flex max-w-3xl flex-col mt-32 font-roboto">
+				<Container className="mx-auto flex max-w-3xl flex-col mt-32 font-roboto leading-4">
 				<div className="grid grid-cols-2 gap-3 text-sm">
 					<div className="border-b-[1px]">
 							<p>
