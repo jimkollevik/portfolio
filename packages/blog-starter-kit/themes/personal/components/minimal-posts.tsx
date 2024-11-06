93aebdf61ddf5379056e3d95a6267f750f78ea07
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-06 19:44:53 +0100

    Update minimal-posts.tsx

--- packages/blog-starter-kit/themes/personal/components/minimal-posts.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-posts.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export const MinimalPosts = ({ posts }: Props) => {
 	return (
-		<div>Hello</div>
 		<section className="min-h-450 mb-6 text-sm">
+			<div>Hello world!</div>
 			{posts.map((post) => (
 				<MinimalPostPreview
 					key={post.id}
