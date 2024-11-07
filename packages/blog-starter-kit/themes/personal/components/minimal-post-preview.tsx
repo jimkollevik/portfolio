d39792e726c1ca5b6ec835b65ce00b21026c9b7b
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-07 22:17:38 +0100

    Update minimal-post-preview.tsx

--- packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
@@ -17,7 +17,7 @@ export const MinimalPostPreview = ({ title, date, slug, commentCount }: Props) =
 
 	return (
 		<section className="flex flex-col items-start gap-1">
-			<h2 className="text-base leading-tight tracking-tight text-black dark:text-white">
+			<h2 className="text-base leading-tight tracking-tight text-black dark:text-white mb-3.5">
 				<Link href={postURL}>{title}</Link>
 			</h2>
 		</section>
