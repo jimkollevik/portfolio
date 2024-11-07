ef0e69295733dcaaba82c700a5c51e5d8e42cba5
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-07 20:38:54 +0100

    Update footer.tsx

--- packages/blog-starter-kit/themes/personal/components/footer.tsx
+++ packages/blog-starter-kit/themes/personal/components/footer.tsx
@@ -4,8 +4,9 @@ export const Footer = () => {
 	const { publication } = useAppContext();
 
 	return (
-		<footer className="border-t pt-10 text-sm text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
-			&copy; {new Date().getFullYear()} {publication.title}
+		<footer className="border-t pt-10 text-base text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
+			<p>Thanks for scrolling all the way down. You can reach me at <a href="">jim.kollevik@gmail.com</a> or read <a href="">my cv here</a>.
+			Special thanks to <a href="https://rsms.me">https://rsms.me</a> for the use of Inter face!</p>
 		</footer>
 	);
 };
