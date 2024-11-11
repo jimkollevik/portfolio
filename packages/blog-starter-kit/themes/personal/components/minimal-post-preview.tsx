3f97f96aa9af4485341ea4ff16e8d7a77df964b9
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-11 20:11:30 +0100

    Update minimal-post-preview.tsx

--- packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
@@ -18,10 +18,11 @@ export const MinimalPostPreview = ({ title, subtitle, date, slug, commentCount }
 
 	return (
 		<section className="flex flex-col items-start gap-1">
-			<h2 className="text-base leading-tight tracking-tight text-black dark:text-white mb-2.5">
-				<Link href={postURL}>{title}</Link>
-			</h2>
-			<p>{subtitle}</p>
+<h2 className="text-base leading-tight tracking-tight text-black dark:text-white mb-2.5">
+  <Link href={postURL}>{title}</Link>
+  <br /> {/* Bryt rad */}
+  <span className="text-sm text-gray-600 dark:text-gray-400">{subtitle}</span>
+</h2>
 		</section>
 	);
 };
