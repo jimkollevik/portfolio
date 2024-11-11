ed894c1f75835ea7347a54245126ea45faac862b
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-11 19:48:09 +0100

    Update minimal-posts.tsx

--- packages/blog-starter-kit/themes/personal/components/minimal-posts.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-posts.tsx
@@ -13,6 +13,7 @@ export const MinimalPosts = ({ posts }: Props) => {
 				<MinimalPostPreview
 					key={post.id}
 					title={post.title}
+					subtitle={post.subtitle}
 					date={post.publishedAt}
 					author={{
 						name: post.author.name,
