caf85e3ae0c1f9598f3f5eaecdd81d5c4b1999f6
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-12 20:10:50 +0100

    typography updates

--- packages/blog-starter-kit/themes/personal/components/footer.tsx
+++ packages/blog-starter-kit/themes/personal/components/footer.tsx
@@ -5,9 +5,6 @@ export const Footer = () => {
 
 	return (
 		<footer className="pt-10 text-base text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
-			<div>
-				<h2 className="text-base text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">Thanks & contact</h2>
-			</div>	
 			<p className="font-light">Thanks for scrolling all the way down. You can reach me at <a href="" className="underline">jim.kollevik@gmail.com</a> or read <a href="" className="underline" target="_blank">my cv here</a>.
 			Special thanks to <a href="https://rsms.me" className="underline" target="_blank">https://rsms.me</a> for the use of Inter face!</p>
 		</footer>
