e4bc1ff39cb1b2c08f3c9be336618d468662f5a0
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-12 21:40:16 +0100

    Update minimal-post-preview.tsx

--- packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
@@ -21,7 +21,7 @@ export const MinimalPostPreview = ({ title, subtitle, date, slug, commentCount }
 			<h2 className="text-sm font-light leading-tight tracking-tight text-black dark:text-white">
 				<Link href={postURL}>{title}</Link>
 			</h2>
-			<p className="font-light mb-2"><DateFormatter dateString={date} /></p>
+			<p className="font-light mb-2 text-accent-1"><DateFormatter dateString={date} /></p>
 		</section>
 	);
 };
