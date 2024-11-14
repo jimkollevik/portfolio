2ea7480acc82fed856bb20dbc7bda75170fa6bd8
Jim Kollevik <jim.kollevik@gmail.com>
2024-11-14 21:22:24 +0100

    wrong dir

--- packages/blog-starter-kit/themes/personal/components/footer.tsx
+++ packages/blog-starter-kit/themes/personal/components/footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
 			<div className="mb-6">
 				<h2 className="text-black dark:text-white">Thanks & contact</h2>
 			</div>	
-			<p className="text-accent-1 font-interLight">You can reach me at <a href="" className="underline">jim.kollevik@gmail.com</a> or read <a href="../assets/cv_eng_jimkollevik.pdf" className="underline" target="_blank">my cv here</a>.
+			<p className="text-accent-1 font-interLight">You can reach me at <a href="" className="underline">jim.kollevik@gmail.com</a> or read <a href="assets/cv_eng_jimkollevik.pdf" className="underline" target="_blank">my cv here</a>.
 			<br/>Special thanks to <a href="https://rsms.me" className="underline" target="_blank">https://rsms.me</a> for the use of Inter face!</p>
 		</footer>
 	);
