4420ad9f79d66acf28d42d252264d024332aa681
Jim Kollevik <jim.kollevik@gmail.com>
2024-12-22 17:18:22 +0100

    Update _document.tsx

--- packages/blog-starter-kit/themes/personal/pages/_document.tsx
+++ packages/blog-starter-kit/themes/personal/pages/_document.tsx
@@ -3,6 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en" className="font-inter">
+			{/* Umami analytics */}
+			<script
+          		defer
+          		src="https://cloud.umami.is/script.js"
+          		data-website-id="da51ba08-993d-4506-bb5c-7ddb5f66e7c6">
+        	</script>
+			
 			<Head />
 			<body>
 				<Main />
